6d49c1a2999e1aedc901b62e4e685354353eb3ba
Jeroen <admin@jeroendev.one>
2020-09-01 17:31:37 +0200

    option gefixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,15 @@
             <h2 class="text-center">&nbsp;Bestel formulier</h2>
             <div class="form-group"><input class="form-control" type="text" name="naam" placeholder="Naam" required></div>
                 <div class="form-group"><input class="form-control" type="email" name="email" placeholder="E-mail adres" required></div>
-            <div class="form-group">
-                <select class="form-control" name="restaurant" id="restaurant">
-                    <option value="" selected disabled hidden>Restaurant</option>
+            <div class="form-group" oninvalid="this.setCustomValidity('Selecteer het restaurant')" required>
+
+                <select class="form-control" name="restaurant" id="restaurant" oninvalid="this.setCustomValidity('Selecteer het restaurant.')" required>
+                    <option value="">Restaurant</option>
                     <option value="De Broodbode">De Broodbode</option>
                     <option value="De Waag">De Waag</option>
                 </select></div>
 
-            <div class="form-group"><input class="form-control" name="bestelling" placeholder="Bestelling" rows="14" oninvalid="this.setCustomValidity('Voer je bestelling in')" required></textarea></div>
+            <div class="form-group"><input class="form-control" name="bestelling" placeholder="Bestelling" rows="14" oninvalid="this.setCustomValidity('Voer je bestelling in.')" required></textarea></div>
             <div class="form-group"><button class="btn btn-primary btn-block" type="submit"><i class="fas fa-check-circle"></i>&nbsp;bestelling plaatsen</button></div>
             @if(session()->has('message'))
             <div class="alert alert-success" role="alert">
